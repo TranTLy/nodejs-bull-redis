3f857cf780165ee859e96326ce44ec61dc96b0a2
TranTLy <tranly237@gmail.com>
2021-06-02 00:09:08 +0700

    example to have luch without drink the water

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const q = new Queue('sample', 'redis://localhost:6379');
 const ACTION = ["LẤY CƠM", "ĂN CƠM", "NHAI", "NUỐT"]
 let currentAction = 0
 
-const doTask = async () => {
-    for (let i = 0; i < 2000; i++) {
+const doTask = (index) => {
+    for (let i = 0; i < 10; i++) {
         process.stdout.write("*");
+        process.stdout.write(index.toString());
     }
 }
 
 const heavyTask = async () => {
     for (let i = 0; i < 200; i++) {
-        await doTask(i)
+        doTask(i)
     }
 }
 
@@ -26,9 +27,6 @@ const init = async () => {
             q.process(taskName, async (job) => {
                 // TODO handle your maintenance operation.
                 console.log(" check to drink water: ", currentAction)
-                if (currentAction === 1) {
-                    console.log('UỐNG NƯỚC')
-                }
             }).catch((e) => console.log(`Something went wrong: ${e}`));
         }, 1000) // every 1 second
     } catch (err) {
